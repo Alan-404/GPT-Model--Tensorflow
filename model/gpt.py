3e6efb3cb2c1cb217d6d253d0de31317ea06f238
Alan Nguyen <nguyentri.alan@gmail.com>
2023-03-26 10:07:02 +0700

    module

--- model/gpt.py
+++ model/gpt.py
@@ -9,6 +9,10 @@ from model.components.decoder import Decoder
 from keras.optimizers import Adam
 from keras.losses import SparseCategoricalCrossentropy
 
+from keras.metrics import Mean
+
+import os
+
 from typing import Callable
 
 class GPTModel(Model):
@@ -53,5 +57,32 @@ class GPT:
 
         self.cross_entropy = SparseCategoricalCrossentropy(from_logits=True, reduction='none')
         self.optimizer = Adam()
+
+        self.checkpoint = tf.train.Checkpoint(model=self.model, optimizer = self.optimizer)
+        self.checkpoint_manager = tf.train.CheckpointManager(self.checkpoint, checkpoint, max_to_keep=3)
+
+        self.train_loss = Mean(name='train_loss')
+
+    def loss_function(self, outputs: Tensor, labels: Tensor):
+        mask = tf.math.logical_not(labels == 0)
+
+        loss = self.cross_entropy(labels, outputs)
+        
+        mask = tf.cast(mask, dtype=loss.dtype)
+        
+        loss = loss*mask
+        
+        return tf.math.reduce_sum(loss) / tf.math.reduce_sum(mask)
+
+    def train_step(self, inputs: Tensor, labels: Tensor):
+        with tf.GradientTape() as tape:
+            outputs = self.model(inputs, True)
+            loss = self.loss_function(outputs, labels)
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
     
-    
+        self.train_loss.update_state(loss)
+
+    """ def fit(self, train_data: Tensor, epochs: int = 1, batch_size: int = 1, mini_batch: int = 1):
+         """
